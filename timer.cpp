64358a1f1df64b33572d3c4822959a2abc328e4f
Mariusz Chilmon <vmario1986@gmail.com>
2024-01-20 20:34:52 +0100

    Remove unused code

--- timer.cpp
+++ timer.cpp
@@ -6,27 +6,17 @@ const Timer timer;
 
 namespace {
 /**
- * Konfiguracja Timer/Counter1.
+ * Preskaler Timer/Counter0.
  */
-constexpr uint8_t TIMER1_MODE = _BV(WGM12);
-
-/**
- * Preskaler Timer/Counter1.
- */
-constexpr uint8_t TIMER1_PRESCALER = _BV(CS11);
+constexpr uint8_t TIMER_PRESCALER = _BV(CS02);
 }
 
 /**
- * Inicjalizuje Timer/Counter1.
+ * Inicjalizuje Timer/Counter0.
  */
 void Timer::initialize() const
 {
-	constexpr double TOP{1.0 * F_CPU / 8 / TIMER_FREQUENCY - 1};
-	static_assert(TOP == static_cast<uint16_t>(TOP),
-		"Cannot obtain the exact frequency.");
-
-	OCR1A = TOP;
-	TCCR1B = TIMER1_PRESCALER | TIMER1_MODE;
-	TIMSK1 = _BV(OCIE1A);
+	TCCR0B = TIMER_PRESCALER;
+	TIMSK0 = _BV(TOIE0);
 }
 
